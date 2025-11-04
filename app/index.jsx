71f2be588470a53490f5d2b77252feaaea07d848
wiissal <wissaloa1@gmail.com>
2025-11-04 22:03:35 +0100

    fix the axios call

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,20 @@
 import { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { fetchAttractions } from '../services/api';
-// Home component to test API integration
+
 export default function Home() {
   useEffect(() => {
-    {/* Test the API call */}
     const testAPI = async () => {
       const data = await fetchAttractions();
       console.log('API Data:', data);
     };
     testAPI();
   }, []);
-// Render a simple view
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Testing API</Text>
     </View>
   );
 }
+
--- services/api.jsx
+++ services/api.jsx
@@ -1,18 +1,13 @@
-
-import React from 'react';
 import axios from 'axios';
 
-const API_URL ="https://690996b92d902d0651b45688.mockapi.io/api/attractions";
+const API_URL = 'https://690996b92d902d0651b45688.mockapi.io/api/attractions';
 
-export const fetchAttractions =  async () => {
+export const fetchAttractions = async () => {
   try {
-    const response = await axios.get (API_URL);
+    const response = await axios.get(API_URL);
     return response.data;
-  }
-  catch (error){
-    console.error('Error fetching attractions' , error);
+  } catch (error) {
+    console.error('Error fetching attractions:', error);
     throw error;
   }
 };
-
-
