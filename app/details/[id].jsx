7abfb21251edfab21b47acaa478152dffe01af90
wiissal <wissaloa1@gmail.com>
2025-11-06 16:36:16 +0100

    info card overlay with responsive design

--- app/details/[id].jsx
+++ app/details/[id].jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect ,useState } from "react";
 import {
   View,
   Text,
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Pressable,
   ActivityIndicator,
+  ScrollView,
 } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import useStore from "../../store/useStore";
@@ -28,6 +29,7 @@ export default function Details() {
       fetchAttractions();
     }
   }, []);
+  const [showFullDescription, setShowFullDescription] = useState(false);
   const attraction = attractions.find((item) => item.id == id);
 
   //adding checks
@@ -49,25 +51,62 @@ export default function Details() {
   }
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
       <Image 
       source={{uri: attraction.thumbnail}}
       style = {styles.heroImage}/>
-      <Text style={styles.title}>{attraction.name}</Text>
-      <Text style={styles.description}>{attraction.description}</Text>
+      <ScrollView style= {styles.infoCard} ContentContainerStyle = {styles.infoCardContent}>
+        <Text style={styles.title}>{attraction.name}</Text>
+        <Text style={styles.rating}>⭐⭐⭐⭐⭐</Text>
+        <Text style={styles.location}>📍 Location here</Text>
+        <Text style={styles.description} numberOfLines={showFullDescription? undefined :3}>{attraction.description}</Text>
+        <Pressable onPress ={() => setShowFullDescription (!showFullDescription)}>
+          <Text style ={styles.readMore}>
+            {showFullDescription ? 'Read Less' : 'Read More'}
+          </Text>
+        </Pressable>
+      </ScrollView>
+      </View>
     </View>
+
 
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
     backgroundColor: "#fff",
+  },
+  imageContainer:{
+    width : '100%',
+    height : 500,
+    position: 'relative',
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
-    marginTop: 50,
+    marginTop: 5,
+  },
+  rating:{
+    fontSize: 14,
+    marginTop : 3,
+    marginBottom : 4,
+  },
+  location:{
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 8,
+  },
+  description:{
+    fontSize: 12,
+    color: '#666',
+    lineHeight: 16,
+  },
+  readMore:{
+    fontSize:12,
+    color : '#555',
+    fontWeight:  '600',
+    marginTop : 5,
   },
   center: {
     flex: 1,
@@ -76,6 +115,26 @@ const styles = StyleSheet.create({
   },
   heroImage : {
     width: '100%',
-    height : 300 ,
-  }
+    height : '100%',
+  },
+  infoCard: {
+    position: 'absolute', //to make it float
+    bottom: 30,
+    left : 20,
+    right: 20,
+    backgroundColor : 'rgba(255 ,255 ,255 , 0.92)',
+    borderRadius : 20,
+    padding : 12,
+    maxHeight: 180,
+    shadowColor : '#000',
+    shadowOffset : {width: 0 , height :2},
+    shadowOpacity : 0.25,
+    shadowRadius : 3.84,
+    elevation: 5, //shadow for androind
+  },
+  infoCardContent:{
+    padding :12,
+  },
+
+
 });
