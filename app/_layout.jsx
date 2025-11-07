2d026a9feabaaaeb8750ceb78a54fad048420dd4
OOLAHIANE MOHAMED <oolahianemohamed1@gmail.com>
2025-11-07 17:27:16 +0100

    Add initial home screen assets

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,14 @@
 import { Stack } from 'expo-router';
 
 export default function StackLayout() {
-  return <Stack />;
+  return ( <Stack>
+    <Stack.Screen
+        name="index"
+        options={{
+          title: "Beni Mellal Tourism",
+          headerShown: false,
+        }}
+      />
+
+    </Stack>);
 }
