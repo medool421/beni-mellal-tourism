4057d88e28ac935218410e4a5122ac6184adc2d4
wiissal <wissaloa1@gmail.com>
2025-11-07 16:36:29 +0100

    adding gallery screen with stack card animation

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,15 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler"; // ADD THIS
 
-export default function StackLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}> 
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Attractions" }} />
+        <Stack.Screen name="details/[id]" options={{ title: "Details" }} />
+        <Stack.Screen name="gallery/[id]" options={{ title: "Gallery" }} />
+        <Stack.Screen name="test-animations" options={{ title: "Test" }} />
+      </Stack>
+    </GestureHandlerRootView> 
+  );
 }
